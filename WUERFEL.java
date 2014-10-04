7d7d29b457535d3349c648d09600aa11bf655db4
Peter <brichzin@gymnasium-ottobrunn.de>
2014-10-04 15:24:33 +0200

    Methode wuerfeln der Klasse WUERFEL

--- WUERFEL.java
+++ WUERFEL.java
@@ -20,14 +20,14 @@ public class WUERFEL
     }
 
     /**
-     * An example of a method - replace this comment with your own
+     * EIn etwas seltsames Wuerfeln
      * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * @return     Wuerfelergebnis zwischen 1 und 6
      */
     public int wuerfeln()
     {
-        // put your code here
-        return 1;
+        x = x+1;
+        if ((x % 6)==0) return 6;
+        else return x % 6;
     }
 }
